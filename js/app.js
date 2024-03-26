d6af9b10f43e16abd8c8d0834ca8d5a456bddc7b
Jorge <rack1992@gmail.com>
2024-03-25 22:53:46 -0500

    test  3

--- js/app.js
+++ js/app.js
@@ -4,8 +4,11 @@ const wasi = "4S5J_vW2T_ZJ1u_PvKR";
 let url = `https://api.wasi.co/v1/location/all-countries?id_company=${id_company}&wasi_token=${wasi}`;
 console.log(url);
 
-let test = fetch("https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR");
-console.log(test);
+fetch(
+  "https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR"
+)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
 async function logMovies() {
   const response = await fetch(
@@ -14,5 +17,3 @@ async function logMovies() {
   const movies = await response.json();
   console.log(movies);
 }
-
-logMovies();
