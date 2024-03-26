18791ee775d9ed751f40f7ac4d53a8366f81e782
Jorge <rack1992@gmail.com>
2024-03-25 22:55:03 -0500

    test

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ fetch(
   .then((response) => response.json())
   .then((json) => console.log(json));
 
-async function logMovies() {
-  const response = await fetch(
-    "https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR"
-  );
-  const movies = await response.json();
-  console.log(movies);
-}
+// async function logMovies() {
+//   const response = await fetch(
+//     "https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR"
+//   );
+//   const movies = await response.json();
+//   console.log(movies);
+// }
