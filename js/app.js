ff0da7cdd58d4dd85df652df4a66f10b968308f0
Jorge <rack1992@gmail.com>
2024-04-01 09:26:29 -0500

    test

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ const wasi = "4S5J_vW2T_ZJ1u_PvKR";
 let url = `https://api.wasi.co/v1/location/all-countries?id_company=${id_company}&wasi_token=${wasi}`;
 console.log(url);
 
+// fetch(
+//   "https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR"
+// )
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
 fetch(
-  "https://api.wasi.co/v1/property/search?id_company=12187774&wasi_token=4S5J_vW2T_ZJ1u_PvKR"
+  "https://jsonplaceholder.typicode.com/posts"
 )
   .then((response) => response.json())
   .then((json) => console.log(json));
